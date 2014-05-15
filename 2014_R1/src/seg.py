20ba26854c1d5cdad01bf49042bd13d03a191105
Ubuntu <ubuntu@ip-10-169-6-200.ap-northeast-1.compute.internal>
2014-05-15 12:25:18 +0000

    change time

--- 2014_R1/src/seg.py
+++ 2014_R1/src/seg.py
@@ -4,11 +4,14 @@
 
 import sys
 import re
-import pickle
+import cPickle as pickle
+import gc
 from pprint import *
+import datetime
 
 VERSION="0.1"
 WORD_MAP_DEPTH=6
+TIME_CHECK=False
 
 def char_model(filename):
   model = {}
@@ -106,12 +109,15 @@ def query_by_char(model, str):
   return result.strip()
 
 def query_by_word(cmodel, wmodel, str):
+  elapsed_time()
   tree = word_tree(cmodel, wmodel, str)
 
+  elapsed_time()
   value, result = search_max(tree, '+')
   if result == '':
+    elapsed_time()
     value, result = search_max(tree, '-')
-
+  elapsed_time()
   return result
 
 def query(cmodel, wmodel, str):
@@ -122,12 +128,16 @@ def query(cmodel, wmodel, str):
   return result
 
 def word_tree(cmodel, wmodel, str):
+  elapsed_time("tree 1")
   tree = {'': str}
   _word_tree(cmodel, tree)
   tree = tree['']
 
+  elapsed_time("tree 2")
   _word_tree_count(wmodel, tree, '')
+  elapsed_time("tree 3")
   _word_tree_count2(wmodel, tree, '', '')
+  elapsed_time("tree 4")
 
   return tree
 
@@ -213,14 +223,21 @@ def _search_max(tree, str, char):
   return max, max_str  
 
 def query_check(cmodel, wmodel, str):
+
   a = str.strip().split('\t')
+ 
+  elapsed_time2(False) 
   b = query(cmodel, wmodel, a[0])
+  print a[0], ", ", len(a[0]), ", ",
+  elapsed_time2(True) 
+  '''
   if b == a[1]:
     print "{} : {} : {} --> PASS".format(a[0], a[1], b)
     return True
   else:
     print "{} : {} : {} --> FAIL".format(a[0], a[1], b)
     return False
+  '''
 
 def query_check_file(cmodel, wmodel, filename):
   pc = 0
@@ -248,6 +265,22 @@ def prefix(model, str):
   print "PREFIX = {}".format(checked)
   pprint(node)
       
+last_time = datetime.datetime.now()
+
+def elapsed_time(str=''):
+  if not TIME_CHECK:
+    return
+  global last_time
+  now = datetime.datetime.now()
+  print str, " : ", now - last_time
+  last_time = now
+
+def elapsed_time2(out):
+  global last_time
+  now = datetime.datetime.now()
+  if out:
+    print (now - last_time).microseconds
+  last_time = now
 
 if __name__ == '__main__':
   import argparse
@@ -263,6 +296,8 @@ if __name__ == '__main__':
   parser.add_argument('--word', action='store_true', help='Show word analysis')
   args = parser.parse_args()
 
+
+  gc.disable()
   if args.data:
     cmodel = char_model(args.data)
     wmodel = word_model(args.data)
@@ -273,6 +308,7 @@ if __name__ == '__main__':
       cmodel = pickle.load(open(args.cmodel, 'rb'))
     if args.wmodel:
       wmodel = pickle.load(open(args.wmodel, 'rb'))
+  gc.enable()
 
   if args.tree:
     tree = word_tree(cmodel, wmodel, args.tree)
@@ -297,6 +333,8 @@ if __name__ == '__main__':
   if args.query:
     query_check_file(cmodel, wmodel, args.query)
   else:
-    pprint(cmodel)
-    pprint(wmodel)
+    #pprint(cmodel)
+    #pprint(wmodel)
     pass
+
+  elapsed_time()
